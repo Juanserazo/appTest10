3b43311c144a518cda6a385c35348d994bd8b1b5
Juanserazo <juanserazo@hotmail.es>
2019-08-12 10:06:28 -0500

    Hola

---
+++ Basic_Calc.py
@@ -0,0 +1,17 @@
+#Developer: Juan Sebastian Erazo
+#Basic Calc
+#Version: 1.0
+
+import os
+
+
+
+a = 10
+b = 5
+add = a + b
+
+#Limpiar pantalla
+os.system("cls")
+
+print(".::Basic Calc::.")
+print("The add is: ", add)
